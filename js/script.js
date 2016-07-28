95e942b3765cead5a9c6e8c74ead7598f2e9cf3c
kendradalley <kendracdalley@gmail.com>
2016-07-27 21:36:46 -0700

    corrected the layout, needs a few jquery updats

--- js/script.js
+++ js/script.js
@@ -10,10 +10,14 @@ var listItem = $('.list-item');
 $('#addItem').on('click', function(){
   var newListItem = $('<li></li>').text(inputText.val());
   var xButton = newListItem.append('<div class=\"deleteButton\">X</div>')
+  var padding = $( 'li' ).css( "padding", "5px" );
+  // var border = $( 'li' ).css( "border-top", "2px solid #000" );
   xButton.on('click', function(){
     $(this).remove();
   });
   $('ul').append(newListItem);
+
+
 });
 
 
